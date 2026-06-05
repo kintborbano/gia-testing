cf8bd2f63e48c0dd8b16335a0983275c5ee0efd4
kintborbano <kint@sofitech.ai>
2026-06-05 12:39:19 +0800

    fix: add more bottom padding to the footer

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -16,7 +16,7 @@ export default function Footer(): React.ReactElement {
   return (
     <footer
       id="bg-stop-footer"
-      className="flex w-full justify-center px-5 pb-20 sm:px-8 md:px-16 md:pb-[120px]"
+      className="flex w-full justify-center px-5 pb-28 sm:px-8 md:px-16 md:pb-[160px]"
     >
       <div className="text-brand-primary flex w-[1056px] max-w-full flex-col gap-[60px] py-16 md:flex-row md:justify-between md:gap-[60px] md:py-20">
         {/* Lead-magnet column */}
